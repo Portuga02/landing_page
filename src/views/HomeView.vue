7dcf541c74c5bb750a9b2fd024000b73a4448d5c
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-05-30 21:36:25 -0300

    ajuste de template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,33 +12,28 @@ export default (await import('vue')).defineComponent({
 })
 
 </script>
+
 <template>
   <div>
     <header>
       <div class="line-contact">
         <div class="container flex justify-content-between">
-          <div class="contacts">
-            <p>
-              <font-awesome-icon class="top-icons" icon="fa-solid fa-phone" /> (81) 3373-2369
-            </p>
-            <p>
-              <font-awesome-icon class="top-icons" icon="fa-solid fa-envelope" /> saviogomes@tisaude.com
-            </p>
-            <p>
-              <font-awesome-icon class="top-icons" icon="fa-solid fa-location-dot" />Rua Matriz de Barros
-            </p>
+          <div class="contacts color-gray-4">
+            <p><font-awesome-icon class="top-icons" :icon="['fas', 'phone']" /> 99999-0326</p>
+            <p><font-awesome-icon class="top-icons" :icon="['fas', 'envelope']" /> savio.gomes@tisaude.com</p>
+            <p><font-awesome-icon class="top-icons" :icon="['fas', 'location-dot']" /> Rua Mariz e Barros, 311</p>
           </div>
           <div class="social-media">
             <font-awesome-icon class="top-icons" :icon="['fab', 'square-facebook']" />
             <font-awesome-icon class="top-icons" :icon="['fab', 'square-twitter']" />
             <font-awesome-icon class="top-icons" :icon="['fab', 'linkedin']" />
             <font-awesome-icon class="top-icons" :icon="['fab', 'square-instagram']" />
-          </div>
-        </div>
+          </div>  
+        </div>        
       </div>
-      <div class="container flex justify-content-between">
+      <div class="container flex justify-content-between align-items-center">
         <div class="brand">
-          <img :src="brand" alt="brand">
+          <img :src="brand" alt="">
         </div>
         <nav>
           <ul class="flex menu">
@@ -50,33 +45,170 @@ export default (await import('vue')).defineComponent({
           </ul>
         </nav>
       </div>
-      <div class="background-header" :style="'background-image:url(' + background + ')'">
-        <div class="shadow">
-          <div class="container-banner flex justify-content-center">
-            <div class="container">
-              <div class="row flex flex-wrap">
-                <div class="col-lg-6 flex flex-direction-column justify-content-center tc-white">
-                  <h1 class="fs-45">Exceptional Health Care for Woman</h1>
-                  <p>Em um dado instante, tudo o que o cirurgião sabe de repente se torna importante para a solução do
-                    problema. Você não pode fazer isso uma hora depois ou amanhã. Você também não pode ir à biblioteca e
-                    pesquisar
-                  </p>
-                  <div class="flex t-margin-3">
-                    <button class="btn-solid-white r-margin-3">Agendamentos</button>
-                    <button class="btn-solid-inline">Saiba Mais</button>
-                  </div>
-                </div>
-                <div class="col-lg-6">
+    </header>
+    <main>
+      <div class="container-full background-header" :style="'background-image: url(' + backheader + ')'">
+        <div class="shadow"></div>
+        <div class="container-banner flex justify-content-center">
+          <div class="container">
+            <div class="row flex flex-wrap">
+              <div class="col-lg-6 -t-margin-90 flex flex-direction-column justify-content-center tc-white">
+                <h1 class="fs-45 b-margin-2">
+                  Cuidados de Saúde Excepcionais para Mulheres
+                </h1>
+                <p class="t-margin-0">
+                  O melhor médico é aquele que mais esperança inspira. A medicina
+                  é o remédio para todas as dores humanas, apenas o amor é um mal
+                  que não tem cura. O melhor médico é aquele que recebe os que
+                  foram desenganados por todos os outros. Medicina é a arte de
+                  compartilhar a vida.
+                </p>
+                <div class="flex t-margin-3">
+                  <button class="btn-solid-white r-margin-3">Agendamentos</button>
+                  <button class="btn-inline-white">Saiba Mais</button>
                 </div>
               </div>
+              <div class="col-lg-6"></div>
             </div>
           </div>
         </div>
       </div>
-    </header>
-    <main>
+      <div class="container flex bg-white -t-margin-90 box-shadow-1 border-radius-10 z-index-9">
+        <div class="box-header flex justify-content-around flex-direction-column">
+          <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'house-chimney-medical']" />
+          <h2 class="t-center tc-purple">850</h2>
+          <p class="t-center fw-bold tc-gray-400">Leitos</p>
+        </div>
+        <div class="box-header flex justify-content-around flex-direction-column">
+          <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'people-group']" />
+          <h2 class="t-center tc-purple">25000+</h2>
+          <p class="t-center fw-bold tc-gray-400">Pacientes Satisfeitos</p>
+        </div>
+        <div class="box-header flex justify-content-around flex-direction-column">
+          <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'user-doctor']" />
+          <h2 class="t-center tc-purple">750</h2>
+          <p class="t-center fw-bold tc-gray-400">Médicos e Enfermeiros</p>
+        </div>
+        <div class="box-header flex justify-content-around flex-direction-column">
+          <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'award']" />
+          <h2 class="t-center tc-purple">18</h2>
+          <p class="t-center fw-bold tc-gray-400">Anos de Mercado</p>
+        </div>
+      </div>
+      <div class="container t-margin-50">
+        <div class="row flex flex-wrap">
+          <div class="col-lg-6 flex flex-direction-column justify-content-center">
+            <div class="box-image-about">
+              <img :src="Doctors" class="doctors-image" alt="" />
+            </div>
+          </div>
+          <div class="col-lg-6 flex flex-direction-column justify-content-center">
+            <div class="doctorAboutDescription">
+              <h1 class="b-margin-1 tc-gray-300">Sobre Nosso Hospital</h1>
+              <p class="line-h-1 b-margin-2 fw-4 tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira. A medicina é
+                o remédio para todas as dores humanas, apenas o amor é um mal que
+                não tem cura. O melhor médico é aquele que recebe os que foram
+                desenganados por todos os outros. Medicina é a arte de
+                compartilhar a vida.
+              </p>
+              <p class="t-margin-2 tc-gray-400">
+                <font-awesome-icon class="tc-purple" :icon="['fas', 'circle-check']"/> Navegue em Nosso Site
+              </p>
+              <p class="t-margin-2 tc-gray-400">
+                <font-awesome-icon class="tc-purple" :icon="['fas', 'circle-check']"/> Escolha entre vários serviços
+              </p>
+              <p class="t-margin-2 tc-gray-400 b-margin-2">
+                <font-awesome-icon class="tc-purple" :icon="['fas', 'circle-check']"/> Feedbacks
+              </p>
+              <button class="btn-solid-primary t-margin-3">Agendamentos</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container t-margin-50">
+        <div class="servico-hospital-title x-padding-1 b-margin-4">
+          <h1 class="b-margin-1 tc-gray-300">Nossos Serviços</h1>
+        </div>   
+        <div class="row flex flex-wrap ">
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'user-doctor']" />
+              <h2 class="t-center tc-purple">Profissionais</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'clipboard-list']" />
+              <h2 class="t-center tc-purple">Diagnóstico</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'hospital']" />
+              <h2 class="t-center tc-purple">Doenças</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2 ">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'tooth']" />
+              <h2 class="t-center tc-purple">Odontologia</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'heart-pulse']" />
+              <h2 class="t-center tc-purple">Cardiologia</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'tablets']" />
+              <h2 class="t-center tc-purple">Farmácia</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'dna']" />
+              <h2 class="t-center tc-purple">Neurologia</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+          <div class="col-lg-3 x-padding-1 b-margin-2">
+            <div class="box-service box-shadow-1 flex justify-content-around flex-direction-column">
+              <font-awesome-icon class="tc-purple fs-30" :icon="['fas', 'truck-medical']" />
+              <h2 class="t-center tc-purple">Ambulância</h2>
+              <p class="t-center fw-bold tc-gray-400">
+                O melhor médico é aquele que mais esperança inspira.
+              </p>
+            </div>          
+          </div>
+        </div>
+      </div>
     </main>
     <footer>
+        
     </footer>
   </div>
 </template>
+
