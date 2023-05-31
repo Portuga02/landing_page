2024a68ef9dbae3a8814f643c5e91b2eea704723
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-05-30 21:36:09 -0300

    ajuste de js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,56 @@ import './assets/config_general.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+  faHouseChimneyMedical,
+  faPeopleGroup,
+  faUserDoctor,
+  faAward,
+  faCircleCheck,
+  faClipboardList,
+  faHospital,
+  faTooth,
+  faHeartPulse,
+  faTablets,
+  faDna,
+  faTruckMedical,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faLinkedin,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
 
-const app = createApp(App)
+library.add(
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+  faFacebookSquare,
+  faTwitterSquare,
+  faLinkedin,
+  faInstagramSquare,
+  faHouseChimneyMedical,
+  faPeopleGroup,
+  faUserDoctor,
+  faAward,
+  faCircleCheck,
+  faClipboardList,
+  faHospital,
+  faTooth,
+  faHeartPulse,
+  faTablets,
+  faDna,
+  faTruckMedical
+);
 
-app.use(router)
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
